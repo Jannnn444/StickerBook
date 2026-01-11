66820d33c8d9873565987d4fc5426f8bb86c7d88
Jannnn444 <yucianhuang@gmail.com>
2026-01-11 18:40:19 +0800

    Initial Commit

---
+++ StickerMoneyBook Watch App/StickerMoneyBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerMoneyBookApp.swift
+//  StickerMoneyBook Watch App
+//
+//  Created by Jan    on 2026/1/11.
+//
+
+import SwiftUI
+
+@main
+struct StickerMoneyBook_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
